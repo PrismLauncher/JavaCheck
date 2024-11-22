9999ff81c4922f5ad1d478acb916cdfa98ea4143
TheKodeToad <TheKodeToad@proton.me>
2024-11-22 11:05:46 +0000

    Convert spaces to tabs

--- src/main/java/org/prismlauncher/javacheck/JavaCheck.java
+++ src/main/java/org/prismlauncher/javacheck/JavaCheck.java
@@ -2,27 +2,27 @@ package org.prismlauncher.javacheck;
 
 public final class JavaCheck {
 
-    public static void main(final String[] args) {
-        if (args.length == 0) {
-            System.err.println("Usage: <properties...>");
-            System.exit(1);
-        }
+	public static void main(final String[] args) {
+		if (args.length == 0) {
+			System.err.println("Usage: <properties...>");
+			System.exit(1);
+		}
 
-        boolean missingSome = false;
+		boolean missingSome = false;
 
-        for (final String property : args) {
-            String propertyValue = System.getProperty(property);
+		for (final String property : args) {
+			String propertyValue = System.getProperty(property);
 
-            if (propertyValue == null) {
-                missingSome = true;
-                continue;
-            }
+			if (propertyValue == null) {
+				missingSome = true;
+				continue;
+			}
 
-            System.out.println(property + '=' + propertyValue);
-        }
+			System.out.println(property + '=' + propertyValue);
+		}
 
-        if (missingSome)
-            System.exit(1);
-    }
+		if (missingSome)
+			System.exit(1);
+	}
 
 }
